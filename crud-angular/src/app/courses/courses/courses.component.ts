dbd7d58a6e15b0e5f481e6f50aa2771fe29864ea
Lucas Souza <lucasp.sdev@gmail.com>
2022-09-18 17:30:31 -0300

    feat: create table component

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ICourse } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  courses: ICourse[] = [
+    {
+      _id: '1',
+      name: 'Angular',
+      category: 'Frontend',
+    },
+  ];
+  displayedColumns = ['name', 'category'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
